eecd666c58a3c7d0095c4fc74580facdf89d4b44
Prasadk1226 <124857435+Prasadk1226@users.noreply.github.com>
2024-09-21 12:35:40 +0530

    session storage added to password

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ form.addEventListener("submit", (event) => {
   let user1 = document.createElement("h3");
   let pswd1 = document.createElement("h3");
   localStorage.setItem("ID", user.value);
-  localStorage.setItem("PASSWORD", pswd.value);
+  sessionStorage.setItem("PASSWORD", pswd.value);
   let x = localStorage.getItem("ID", user.value);
-  let y = localStorage.getItem("PASSWORD", pswd.value);
-  user1.innerHTML = "You have entered your USERID as " + x.toUpperCase();
+  let y = sessionStorage.getItem("PASSWORD", pswd.value);
+  user1.innerHTML = "You have entered your USERID as " + x.toUpperCase()+"  & it is stored in the Local storage";
   user1.style.textAlign = "center";
-  pswd1.innerHTML = "You have entered your PASSWORD as " + y.toUpperCase();
+  pswd1.innerHTML = "You have entered your PASSWORD as " + y.toUpperCase()+" & it is stored in the Session storage";
   pswd1.style.textAlign = "center";
   document.body.appendChild(user1);
   document.body.appendChild(pswd1);
